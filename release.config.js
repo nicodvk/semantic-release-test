c6f6f86415585822057b9efc50dad74d5c246f60
Nicolas Devynck <nicolas.devynck@ext.adeo.com>
2023-06-30 15:43:16 +0200

    ci: fix package.json version setter

--- release.config.js
+++ release.config.js
@@ -3,6 +3,8 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
     ['@semantic-release/git', {
       'assets': ['package.json'],
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
